46149c62fe7b19f529131e43020e8d3cf4a21513
Leonor <ldominoni@gmail.com>
2023-02-26 21:17:37 -0300

    sound

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-function  tocaSomDo(){
-    document.querySelector('#som_tecla_do').play();
+function  tocaSom(audioNota){
+    const elemento = document.querySelector(audioNota);
+
+    if(elemento === null){
+        console.log ('Nota não encontrada')
+    } else{
+        elemento.play();
+    }
+
 }
 
-document.querySelector('.tecla_do').onclick = tocaSomDo;
+const listaDeNotas = document.querySelectorAll('.tecla');
+
+for (let cont = 0; cont < listaDeNotas.length; cont++){
+    const tecla = listaDeNotas[cont];
+    const nota = tecla.classList[1];
+    const audioNota = `#som_${nota}`;
 
 
+    tecla.onclick = function (){
+        tocaSom(`#som_${nota}`);
+    }
+
+    tecla.onkeydown = function (evento) {
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
